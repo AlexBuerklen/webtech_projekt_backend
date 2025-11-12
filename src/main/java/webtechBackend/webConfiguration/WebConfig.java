a359e7e2c690b782f43840a75e8cbb724604875d
AlexBuerklen <alexanderbuerklen99@gmail.com>
2025-11-12 18:39:40 +0100

    adapted README and configured WebConfig

--- src/main/java/webtechBackend/webConfiguration/WebConfig.java
+++ src/main/java/webtechBackend/webConfiguration/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://*.amplifyapp.com", "http://localhost:5173")
+                .allowedOriginPatterns("http://localhost:5173")
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*");
     }
